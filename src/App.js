32a79dd65dd2a9ecfbcd46142cb59f3b94feeb84
Yijun <yijun.pan.stautland@tietoevry.com>
2022-04-01 14:23:39 +0200

    Clean up unused logo

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <section>
-        <h1>Level 1</h1>
+        <h1>SCORE 1</h1>
       </section>
       <section>
-        <h1>Level 2</h1>
+        <h1>SCORE 2</h1>
       </section>
       <section>
-        <h1>Level 3</h1>
+        <h1>SCORE 3</h1>
       </section>
       <section>
-        <h1>Level 4</h1>
+        <h1>SCORE 4</h1>
       </section>
     </div>
   );
