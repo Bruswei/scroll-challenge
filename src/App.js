0be51afd8854a89fd5123693cbc5b4a616091668
Yijun <yijun.pan.stautland@tietoevry.com>
2022-04-01 10:13:30 +0200

    Add initial css for snap

--- src/App.js
+++ src/App.js
@@ -4,20 +4,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section>
+        <h1>Level 1</h1>
+      </section>
+      <section>
+        <h1>Level 2</h1>
+      </section>
+      <section>
+        <h1>Level 3</h1>
+      </section>
+      <section>
+        <h1>Level 4</h1>
+      </section>
     </div>
   );
 }
